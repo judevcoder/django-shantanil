5fe9e1ba615b02d71824a3be064ef8290fe314b6
softdevstorm <softdevstorm@gmail.com>
2018-10-05 04:58:43 +0300

    database update

--- dashboard/models.py
+++ dashboard/models.py
@@ -14,7 +14,7 @@ class UserLogger(models.Model):
     comments = models.TextField()
     is_session_active = models.BooleanField(default=False)
     created_ts = models.DateTimeField(auto_now_add=True)
-    updated_ts = models.DateTimeField(null=True)
+    updated_ts = models.DateTimeField(auto_now=True)
     created_by = models.DateField(auto_now_add=True)
 
     class Meta:
@@ -31,7 +31,7 @@ class SupportDriver(models.Model):
     driver_icon_path = models.FilePathField()
     driver_for_source_type = models.CharField(max_length=50)
     created_ts = models.DateTimeField(auto_now_add=True)
-    updated_ts = models.DateTimeField(null=True)
+    updated_ts = models.DateTimeField(auto_now=True)
     created_by = models.DateField(auto_now_add=True)
 
     class Meta:
@@ -44,7 +44,7 @@ class InstalledDriver(models.Model):
     supported_driver_seq = models.ForeignKey(SupportDriver, on_delete=models.CASCADE)
     installed_drivers_name = models.CharField(max_length=100)
     created_ts = models.DateTimeField(auto_now_add=True)
-    updated_ts = models.DateTimeField(null=True)
+    updated_ts = models.DateTimeField(auto_now=True)
     created_by = models.DateField(auto_now_add=True)
 
     class Meta:
@@ -62,7 +62,7 @@ class DataStore(models.Model):
     datastore_connection_type = models.CharField(max_length=50)
     ec_connection_success_flag = models.BooleanField(default=False)
     created_ts = models.DateTimeField(auto_now_add=True)
-    updated_ts = models.DateTimeField(null=True)
+    updated_ts = models.DateTimeField(auto_now=True)
     created_by = models.DateField(auto_now_add=True)
 
     class Meta:
@@ -83,7 +83,7 @@ class EstablishedConnections(models.Model):
     ec_connection_type = models.CharField(max_length=50)
     ec_connection_success_flag = models.BooleanField(default=False)
     created_ts = models.DateTimeField(auto_now_add=True)
-    updated_ts = models.DateTimeField(null=True)
+    updated_ts = models.DateTimeField(auto_now=True)
     created_by = models.DateField(auto_now_add=True)
 
     class Meta:
@@ -101,7 +101,7 @@ class EstablishedSource(models.Model):
     es_connect_string = models.CharField(max_length=100)
     es_is_active = models.BooleanField(default=False)
     created_ts = models.DateTimeField(auto_now_add=True)
-    updated_ts = models.DateTimeField(null=True)
+    updated_ts = models.DateTimeField(auto_now=True)
     created_by = models.DateField(auto_now_add=True)
 
     class Meta:
@@ -109,4 +109,114 @@ class EstablishedSource(models.Model):
         db_table = 'cdg_established_sources'
 
 
+class EstablishedTarget(models.Model):
+    established_target_seq = models.IntegerField(primary_key=True)
+    et_established_connections_seq = models.ForeignKey(EstablishedConnections, on_delete=models.CASCADE)
+    et_source_name = models.CharField(max_length=50)
+    et_type = models.CharField(max_length=50)
+    et_schema = models.CharField(max_length=100)
+    et_directory = models.CharField(max_length=200)
+    et_connect_string = models.CharField(max_length=100)
+    et_is_active = models.BooleanField(default=False)
+    created_ts = models.DateTimeField(auto_now_add=True)
+    updated_ts = models.DateTimeField(auto_now=True)
+    created_by = models.DateField(auto_now_add=True)
 
+    class Meta:
+        managed = True
+        db_table = 'cdg_established_target'
+
+class ContentType(models.Model):
+    content_type_seq = models.IntegerField(primary_key=True)
+    content_type_name = models.CharField(max_length=50)
+    content_type_short = models.CharField(max_length=100)
+    content_type_long_description = models.TextField()
+    content_type_is_active = models.BooleanField(default=False)
+    created_ts = models.DateTimeField(auto_now_add=True)
+    updated_ts = models.DateTimeField(auto_now=True)
+    created_by = models.DateField(auto_now_add=True)
+
+    class Meta:
+        managed = True
+        db_table = 'cdg_content_type'
+
+
+class DataSet(models.Model):
+    dataset_seq = models.IntegerField(primary_key=True)
+    dataset_name = models.CharField(max_length=50)
+    dataset_established_sources_seq = models.ForeignKey(EstablishedSource, on_delete=models.CASCADE)
+    dataset_content_type_seq = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    dataset_description = models.TextField()
+    dataset_is_active = models.BooleanField(default=False)
+    created_ts = models.DateTimeField(auto_now_add=True)
+    updated_ts = models.DateTimeField(auto_now=True)
+    created_by = models.DateField(auto_now_add=True)
+
+    class Meta:
+        managed = True
+        db_table = 'cdg_dataset'
+
+
+class DatasetAttribute(models.Model):
+    dataset_attribute_seq = models.IntegerField(primary_key=True)
+    da_dataset_seq = models.ForeignKey(DataSet, on_delete=models.CASCADE)
+    content_type_seq = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    dataset_attribute_name = models.CharField(max_length=100)
+    da_source_col_seq = models.IntegerField()
+    da_source_col_name = models.CharField(max_length=100)
+    da_sample_col_data = models.CharField(max_length=100)
+    da_source_col_type = models.CharField(max_length=100)
+    is_pii_yn = models.CharField(max_length=100)
+    da_source_col_size_1 = models.CharField(max_length=50)
+    da_source_col_size_2 = models.CharField(max_length=50)
+    da_source_col_short_desc = models.CharField(max_length=200)
+    da_is_active = models.BooleanField(default=False)
+    dataset_attributes_is_profiled = models.BooleanField(default=False)
+    created_ts = models.DateTimeField(auto_now_add=True)
+    updated_ts = models.DateTimeField(auto_now=True)
+    created_by = models.DateField(auto_now_add=True)
+
+    class Meta:
+        managed = True
+        db_table = 'cdg_dataset_attribute'
+
+
+class CurrentSchedule(models.Model):
+    cs_seq = models.IntegerField(primary_key=True)
+    cs_name = models.CharField(max_length=100)
+    cs_command = models.CharField(max_length=200)
+    cs_parameters = models.CharField(max_length=100)
+    cs_runtime = models.CharField(max_length=50)
+    cs_frequency = models.CharField(max_length=100)
+    cs_description = models.TextField()
+    cs_is_active = models.  BooleanField(default=False)
+    created_ts = models.DateTimeField(auto_now_add=True)
+    updated_ts = models.DateTimeField(auto_now=True)
+    created_by = models.DateField(auto_now_add=True)
+
+    class Meta:
+        managed = True
+        db_table = 'cdg_current_schedules'
+
+
+class CurrentScheduleActivities(models.Model):
+    csa_seq = models.IntegerField(primary_key=True)
+    cs_seq = models.ForeignKey(CurrentSchedule, on_delete=models.CASCADE)
+    csa_name = models.CharField(max_length=100)
+    csa_command = models.CharField(max_length=200)
+    csa_parameters = models.CharField(max_length=100)
+    csa_runtime = models.CharField(max_length=50)
+    csa_frequency = models.CharField(max_length=100)
+    csa_jobstatus = models.CharField(max_length=30)
+    csa_job_starttime = models.DateTimeField(auto_now_add=True)
+    csa_job_endtime = models.DateTimeField(auto_now=True)
+    csa_job_run_duration = models.DateTimeField()
+    csa_run_errors = models.CharField(max_length=200)
+    csa_run_output = models.CharField(max_length=200)
+    created_ts = models.DateTimeField(auto_now_add=True)
+    updated_ts = models.DateTimeField(auto_now=True)
+    created_by = models.DateField(auto_now_add=True)
+
+    class Meta:
+        managed = True
+        db_table = 'cdg_current_schedules_activities'
